17737b8f2a1aabb2b741da1b851ffde9b7c485bf
v0agent <it+v0agent@vercel.com>
2026-06-03 16:39:15 +0000

    feat: add OurTeamSection and refactor AboutSection content

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { HeroSection } from '@/components/hero-section'
 import { StatisticsDashboard } from '@/components/statistics-dashboard'
 import { AboutSection } from '@/components/about-section'
+import { OurTeamSection } from '@/components/our-team-section'
 
 export default function HomePage() {
   return (
@@ -8,6 +9,7 @@ export default function HomePage() {
       <HeroSection />
       <StatisticsDashboard />
       <AboutSection />
+      <OurTeamSection />
     </>
   )
 }
