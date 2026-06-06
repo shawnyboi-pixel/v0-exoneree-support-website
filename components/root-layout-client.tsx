ff7dafabe40cf17afa28ecae8d45672b20fbe717
v0agent <it+v0agent@vercel.com>
2026-06-06 15:58:26 +0000

    refactor: remove welcome screen and associated logic from root layout

--- components/root-layout-client.tsx
+++ components/root-layout-client.tsx
@@ -1,41 +1,9 @@
 'use client'
 
-import { useState, useEffect } from 'react'
-import { WelcomeScreen } from './welcome-screen'
-import { getPreference } from '@/hooks/use-user-preference'
-
 interface RootLayoutClientProps {
   children: React.ReactNode
 }
 
 export function RootLayoutClient({ children }: RootLayoutClientProps) {
-  const [showWelcome, setShowWelcome] = useState(false)
-  const [isLoaded, setIsLoaded] = useState(false)
-
-  useEffect(() => {
-    // Check if user has a preference set
-    const preference = getPreference()
-    
-    // Only show welcome if NO preference is set
-    if (!preference) {
-      setShowWelcome(true)
-    }
-    
-    setIsLoaded(true)
-  }, [])
-
-  const handleDismissWelcome = () => {
-    setShowWelcome(false)
-  }
-
-  if (!isLoaded) {
-    return <>{children}</>
-  }
-
-  return (
-    <>
-      {showWelcome && <WelcomeScreen onDismiss={handleDismissWelcome} />}
-      {children}
-    </>
-  )
+  return <>{children}</>
 }
